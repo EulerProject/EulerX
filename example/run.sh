ced1b052801288990d191f1e09a8e638a34f3e0f
Mingmin Chen <mingminchen@users.noreply.github.com>
2012-10-10 23:39:00 -0700

    split src

--- example/run.sh
+++ example/run.sh
@@ -4,9 +4,9 @@ mkdir output/$1
 TEST=$1
 if [ $# -eq 1 ]
 then
-python ../src/main.py -i $1.txt -r $1 -m output/$1  -v "ncd" -n "all_articulations" -T 10 -w "possible"  -h -o done_$1.html 
+python ../src-ct/main.py -i $1.txt -r $1 -m output/$1  -v "ncd" -n "all_articulations" -T 10 -w "possible"  -h -o done_$1.html 
 else
 shift
-python ../src/main.py -i $TEST.txt -r $TEST -m output/$TEST  -v "ncd" -n "all_articulations" -T 10 -w "possible"  -h -o done_$TEST.html $@
+python ../src-ct/main.py -i $TEST.txt -r $TEST -m output/$TEST  -v "ncd" -n "all_articulations" -T 10 -w "possible"  -h -o done_$TEST.html $@
 fi
 ./dot.sh output/$TEST
