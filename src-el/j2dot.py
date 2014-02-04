6ac77d217d99566013e9538d7bbe92804706b9e8
pkianmajd <parisa.kianmajd@gmail.com>
2014-02-04 11:14:53 -0800

    new stylesheet format

--- src-el/j2dot.py
+++ src-el/j2dot.py
@@ -1,74 +1,57 @@
 import json
-import random
 from collections import defaultdict
-from pprint import pprint
 
 nodes = defaultdict(list)
 edges = defaultdict(list)
-cluster = {}
 
 # parse the stylesheet file
-def readStyleSheet(stylefile):
+def read_stylesheet(stylefile):
     global styles
     with open(stylefile) as style_file:    
         styles = json.load(style_file)
-
+        
 def parse(datafile, output):
     f= open(output,"w")
     f.write("digraph{\n")
-    with open('data.json') as data_file:    
+    f.write(str(styles["graphstyle"]["graph"]))
+    with open(datafile) as data_file:    
         data = json.load(data_file)
-        f.write('rankdir=' + styles["Graph"]["rankdir"] + '\n')
-        f.write('labelloc=' + styles["Graph"]["labelloc"] + '\n')
-        f.write('labeljust=' + styles["Graph"]["labeljust"] + '\n')
-        f.write('fontsize=' + styles["Graph"]["fontsize"] + '\n')
-        f.write('label="' + styles["Graph"]["label"] + '"\n')
-        # seperate different node, edge, cluster, ... objects
-        for item, attr in data.iteritems():
-            if data[item]["type"] == "node":
-                nodes[data[item]['group']].append(data[item]["concept"])
-            elif data[item]["type"] == "edge":
-                key = attr.items()
-                edges[data[item]['l']].append(key)
-            elif data[item]["type"] == "cluster":
-                cluster[item]= attr
+    #separate nodes from edges
+        for key, value in data.iteritems():
+            if data[key]["type"] == "node":
+                nodes[data[key]['group']].append(data[key]["concept"])
+            elif data[key]["type"] == "edge":
+                edges[data[key]['l']].append(value.items())
     for g in nodes:
-        f.write('node[shape="' +styles[g]["shape"] + '", style="' +styles[g]["style"] + '", color="' + styles[g]["color"] + '", fillcolor="' + styles[g]["fillcolor"] +'"]' +'\n')
+    # if the style is not defined use the default
+        if g in styles["nodestyle"]:
+            group = g
+        else:
+            group = "default"
+        f.write('node[' + styles["nodestyle"][group] + '] \n')
         for n in nodes[g]:
-            if g!= "common":
+            if (g!= "common" and g!= "none"):
                f.write('"'+ g + "." + n + '"\n')
             else:
-               f.write(n + '\n')           
-    for key, value in cluster.iteritems():
-       r = lambda: random.randint(0,255)
-       color = "#" + hex(r())[2:] + hex(r())[2:] + hex(r())[2:]
-       f.write('subgraph cluster' + str(random.randint(0,100)) + '{\n')
-       f.write('label=""\n')
-       f.write('color="' + color + '"\n')
-       c = cluster[key]['s'].split(",")
-       for i in range(0, len(c)):
-           f.write('"' + c[i] + '"\n')
-       f.write('}\n')
-       f.write('edge[style="' + styles["lsum"]["style"] + '", color="' + color + '", penwidth="' + cluster[key]["w"] + '"]' +'\n')
-       f.write('"' + c[1] + '" -> "' + cluster[key]['t'] + '" [label="' + cluster[key]["l"] + '"]\n')
+               f.write('"'+ n + '"\n')
     for l in edges:
-        if l in styles:
-            f.write('edge[style="' + styles[l]["style"] + '", color="' + styles[l]["color"] + '"]\n')
+        if l in styles["edgestyle"]:
+            label = l
         else:
-            f.write('edge[style="' + styles["default"]["style"] + '", color="' + styles["default"]["color"] + '"]\n')
+            label = "default"
+        f.write('edge['+ styles["edgestyle"][label] +']\n')
         for e in edges[l]:
-            f.write('penwidth="' + e[3][1] + '"\n')
             f.write('"' + e[0][1] + '" -> "' + e[2][1] + '"')
-            if l!= "isa":
+            f.write('[penwidth=' + e[3][1] + ']')
+        # some of the labels are not shown in the output
+            if (l!= "isa"and l!="inplus" and l!="outplus"):
                 f.write(' [label="' + l + '"]')
             f.write('\n')
     f.write('}')            
     f.close()
     
-readStyleSheet('stylesheet.json')
+read_stylesheet('stylesheet.json')
 parse('data.json', 'out.dot')
-
-
 
 
 
