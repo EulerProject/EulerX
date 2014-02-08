c5856bca7bf609689e1c71e1bfb974e8f545d17d
pkianmajd <parisakianmajd@users.noreply.github.com>
2014-02-07 18:40:28 -0800

    parser options

--- src-el/j2dot.py
+++ src-el/j2dot.py
@@ -1,4 +1,5 @@
 import json
+import optparse
 from collections import defaultdict
 
 nodes = defaultdict(list)
@@ -8,9 +9,8 @@ edges = defaultdict(list)
 def read_stylesheet(stylefile):
     global styles
     with open(stylefile) as style_file:    
-        styles = json.load(style_file)
-        
-def parse(datafile, output):
+        styles = json.load(style_file)      
+def apply_style(datafile, output):
     f= open(output,"w")
     f.write("digraph{\n")
     f.write(str(styles["graphstyle"]["graph"]))
@@ -18,44 +18,64 @@ def parse(datafile, output):
         data = json.load(data_file)
     #separate nodes from edges
         for key, value in data.iteritems():
-            if data[key]["type"] == "node":
-                nodes[data[key]['group']].append(data[key]["concept"])
-            elif data[key]["type"] == "edge":
-                edges[data[key]['l']].append(value.items())
+            if "group" in data[key]:
+                nodes[data[key]["group"]].append(data[key]["concept"])
+            elif "label" in data[key]:
+                edges[data[key]["label"]].append(value.items())
     for g in nodes:
     # if the style is not defined use the default
         if (styles["graphstyle"]["subgraph"] == "on" and g!="none"):
-            f.write('subgraph cluster' + g + '{ label= "" style=invis\n')
+            f.write("subgraph cluster" + g + '{ label= "" style=invis\n')
         if g in styles["nodestyle"]:
             group = g
         else:
             group = "default"
-        f.write('node[' + styles["nodestyle"][group] + '] \n')
+        f.write("node[" + styles["nodestyle"][group] + '] \n')
         for n in nodes[g]:
             if (g!= "common" and g!= "none"):
                f.write('"'+ g + "." + n + '"\n')
             else:
                f.write('"'+ n + '"\n')
-        if (styles["graphstyle"]["subgraph"] == "on"and g!="none"):
-            f.write('}')
+        if (styles["graphstyle"]["subgraph"] == "on"and g != "none"):
+            f.write("}")
     for l in edges:
         if l in styles["edgestyle"]:
             label = l
         else:
             label = "default"
-        f.write('edge['+ styles["edgestyle"][label] +']\n')
+        f.write("edge["+ styles["edgestyle"][label] +"]\n")
         for e in edges[l]:
-            f.write('"' + e[0][1] + '" -> "' + e[2][1] + '"')
-            f.write('[penwidth=' + e[3][1] + ']')
+            f.write('"' + e[0][1] + '" -> "' + e[1][1] + '"')
+            f.write('[penwidth=' + e[2][1] + "]")
         # some of the labels are not shown in the output
-            if (l!= "isa"and l!="inplus" and l!="outplus"):
+            if (l != "isa"and l != "inplus" and l != "outplus"):
                 f.write(' [label="' + l + '"]')
-            f.write('\n')
-    f.write('}')            
+            f.write("\n")
+    f.write("}")            
     f.close()
     
-read_stylesheet('stylesheet.json')
-parse('data.json', 'out.dot')
+def parse_options():
+	# parse options
+	parser = optparse.OptionParser(usage = "%prog [options]", version = "%prog 0.1")
+	parser.add_option("-s","--sfile",type="string",dest="sfile",
+					  default=None,
+					  help="file")
+	parser.add_option("-d","--dfile",type="string",dest="dfile",
+					  default=None,
+					  help="file")
+	(options,args) = parser.parse_args()
+	return (options,args)
+			
+if __name__ == '__main__':
+	(options,args) = parse_options()
+	if options.sfile == None:
+		options.sfile = "stylesheet.json"
+	if options.dfile == None:
+		options.dfile = "data.json"
+	read_stylesheet(options.sfile)
+	apply_style(options.dfile, "out.dot")
+
+
 
 
 
