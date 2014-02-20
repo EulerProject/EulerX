5bf49723a043f81a0b81136fefe91a07cc0e8676
Shizhuo Yu <shizhuoyu@gmail.com>
2014-02-20 01:01:47 -0700

    bbox lattice upgrade

--- bbox-lattice/lattice.sh
+++ bbox-lattice/lattice.sh
@@ -1,8 +1,12 @@
+echo "preprocessing..."
+python preprocess.py $1;
 echo "saving all worlds...";
 python awf.py i,o expWorlds.asp;
 echo "creating the lattice without color...";
-#dlv -silent -filter=up  wexp-up.asp expWorlds_aw.asp;
-echo "running euler to get MIS";
+dlv -silent -filter=up  wexp-up.asp expWorlds_aw.asp > up.dlv;
+echo "running euler to get MIS...";
 euler -i $1.txt -e mnpw --ie > output.txt;
-echo "from MIS to MAC and get lattice"
+echo "from MIS to MAC and get lattice..."
 python lattice.py $1;
+dot -Tpdf $1_lat.dot -o $1_lat.pdf;
+echo "finish";
