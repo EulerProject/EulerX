5a27fac701d3073c1e7e34aa67dfbb4d50b6b0a7
Shizhuo Yu <shizhuoyu@users.noreply.github.com>
2014-07-01 10:34:23 -0700

    fixed save all worlds bug

--- bbox-lattice/lattice.sh
+++ bbox-lattice/lattice.sh
@@ -1,7 +1,7 @@
 echo "preprocessing..."
 python preprocess.py $1;
 echo "saving all worlds...";
-python awf.py i,o expWorlds.asp;
+python awf.py -filter=i,o expWorlds.asp;
 echo "creating the lattice without color...";
 dlv -silent -filter=up  wexp-up.asp expWorlds_aw.asp > up.dlv;
 echo "running euler to get MIS...";
