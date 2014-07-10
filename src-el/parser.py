fcef2f6a7eba40dddabec03f896c6ba7a7eb3727
Mingmin Chen <mikemmchen@gmail.com>
2014-07-10 00:03:03 -0700

    remove global in parser.py

--- src-el/parser.py
+++ src-el/parser.py
@@ -5,6 +5,7 @@ class CtiParser:
 
     inst = None
 
+    @Callable
     def instance():
         if CtiParser.inst is None:
             CtiParser.inst = OptionParser(usage="usage: %prog [options]",
@@ -12,71 +13,66 @@ class CtiParser:
             CtiParser.inst.add_option("-v", action="store_true", dest="verbose")
 	return CtiParser.inst
 
-    instance = Callable(instance)
-
 class EulerParser:
 
-    global inst
     inst = None
 
+    @Callable
     def instance():
-        global inst
-        if inst is None:
-            inst = OptionParser(usage="usage: %prog [options]",
+        if EulerParser.inst is None:
+            EulerParser.inst = OptionParser(usage="usage: %prog [options]",
                                 version="%prog 1.0")
-            inst.add_option("-v", action="store_true", dest="verbose",\
-                                  help="verbose mode")
-            inst.add_option("-p", dest="projectname", help="project name")
-            inst.add_option("-i", dest="inputfile", help="input file")
-            inst.add_option("-r", dest="inputdir", default="",\
-                                  help="input directory, by default ./")
-            inst.add_option("-o", dest="outputdir", default=None,\
-                                  help="output directory, by default ./")
-            inst.add_option("-e", dest="encode", default=0, \
-                                  help="encoding, e.g. mnpw, drpw, dlpw, etc.")
-            inst.add_option("-b", dest="dl", default=2, help="dl encoding spare parameter")
-            inst.add_option("--reasoner", dest="reasoner", type="string", default="dlv",\
-                                  help="choose a reasoner")
-            inst.add_option("--repair", dest="repair", type="string", default="topdown",\
-                                  help="choose a repairing approach in case of inconsistency")
-            inst.add_option("--all", action="store_true", dest="allRelations", default=False,\
-                                    help = "generate all the relations including intra-/inter-"\
-                                         + "taxonomies (by default, inter-taxonomy only)")
-            inst.add_option("--cc", action="store_true", dest="consCheck", default=False,\
-                                    help="consistency check only")
-            inst.add_option("--dc", action="store_false", dest="enableCov", default=True,\
-                                    help="disable coverage")
-            inst.add_option("--ur", action="store_true", dest="reduction", default=False,\
-                                  help="turn on uncertainty reduction")
-            inst.add_option("--ho", action="store_true", dest="hideOverlaps", default=False,\
-                                  help="hide the concepts that are involved in overlapping, used with cb encoding")
-            inst.add_option("--ie", action="store_true", dest="ie", default=False,\
-                                    help="inconsistency explanation")
-            inst.add_option("--ieo", action="store_true", dest="ieo", default=False,\
-                                    help="white-box provenance")
-            inst.add_option("--countmir", action="store_true", dest="countOn", default=False,\
-                                  help="count # of occurance for each of rcc5")
-            inst.add_option("--rcgo", action="store_true", dest="rcgo", default=False,\
-                                  help="Reduced containment graphs with overlaps")
-            inst.add_option("--pwcluster", action="store_true", dest="cluster", default=False,\
-                                  help="output the distance between pairwise possible world")
-            inst.add_option("--simpcluster", action="store_true", dest="simpCluster", default=False,\
-                                  help="simplify the possible world cluster")
-            inst.add_option("-N", action="store_false", dest="output", default=True,\
-                                  help="no output")
-            inst.add_option("-g", action="store_true", dest="generateCti", default=False,\
-                                  help="artifitial example generator, cannot be used with -i option")
-            inst.add_option("-n", dest="nary", type="int", default=0, help="N-nary, used with -g")
-            inst.add_option("-m", dest="nnodes", type="int", default=0, help="#nodes, used with -g")
-            inst.add_option("-d", dest="depth", type="int", default=2, help="depth, used with -g")
-            inst.add_option("-t", dest="relation", type="string", default="<",\
-                                  help="artifitial articulaiton rel, used with -g")
-            inst.add_option("-I", action="store_true", dest="incEx", default=False,\
-                                  help="generate an inconsistent example, used with -g")
-            inst.add_option("--iv", action="store_true", dest="inputViz", default=False,\
-                                  help="input visualization")
-            inst.add_option("--simpall", action="store_true", dest="simpAllView", default=False,\
+            EulerParser.inst.add_option("-v", action="store_true", dest="verbose",\
+                                        help="verbose mode")
+            EulerParser.inst.add_option("-p", dest="projectname", help="project name")
+            EulerParser.inst.add_option("-i", dest="inputfile", help="input file")
+            EulerParser.inst.add_option("-r", dest="inputdir", default="",\
+                                         help="input directory, by default ./")
+            EulerParser.inst.add_option("-o", dest="outputdir", default=None,\
+                                              help="output directory, by default ./")
+            EulerParser.inst.add_option("-e", dest="encode", default=0, \
+                                              help="encoding, e.g. mnpw, drpw, dlpw, etc.")
+            EulerParser.inst.add_option("-b", dest="dl", default=2, help="dl encoding spare parameter")
+            EulerParser.inst.add_option("--reasoner", dest="reasoner", type="string", default="dlv",\
+                                              help="choose a reasoner")
+            EulerParser.inst.add_option("--repair", dest="repair", type="string", default="topdown",\
+                                              help="choose a repairing approach in case of inconsistency")
+            EulerParser.inst.add_option("--all", action="store_true", dest="allRelations", default=False,\
+                                                help = "generate all the relations including intra-/inter-"\
+                                                     + "taxonomies (by default, inter-taxonomy only)")
+            EulerParser.inst.add_option("--cc", action="store_true", dest="consCheck", default=False,\
+                                                help="consistency check only")
+            EulerParser.inst.add_option("--dc", action="store_false", dest="enableCov", default=True,\
+                                                help="disable coverage")
+            EulerParser.inst.add_option("--ur", action="store_true", dest="reduction", default=False,\
+                                              help="turn on uncertainty reduction")
+            EulerParser.inst.add_option("--ho", action="store_true", dest="hideOverlaps", default=False,\
+                                              help="hide the concepts that are involved in overlapping, used with cb encoding")
+            EulerParser.inst.add_option("--ie", action="store_true", dest="ie", default=False,\
+                                                help="inconsistency explanation")
+            EulerParser.inst.add_option("--ieo", action="store_true", dest="ieo", default=False,\
+                                                help="white-box provenance")
+            EulerParser.inst.add_option("--countmir", action="store_true", dest="countOn", default=False,\
+                                              help="count # of occurance for each of rcc5")
+            EulerParser.inst.add_option("--rcgo", action="store_true", dest="rcgo", default=False,\
+                                              help="Reduced containment graphs with overlaps")
+            EulerParser.inst.add_option("--pwcluster", action="store_true", dest="cluster", default=False,\
+                                              help="output the distance between pairwise possible world")
+            EulerParser.inst.add_option("--simpcluster", action="store_true", dest="simpCluster", default=False,\
+                                              help="simplify the possible world cluster")
+            EulerParser.inst.add_option("-N", action="store_false", dest="output", default=True,\
+                                              help="no output")
+            EulerParser.inst.add_option("-g", action="store_true", dest="generateCti", default=False,\
+                                              help="artifitial example generator, cannot be used with -i option")
+            EulerParser.inst.add_option("-n", dest="nary", type="int", default=0, help="N-nary, used with -g")
+            EulerParser.inst.add_option("-m", dest="nnodes", type="int", default=0, help="#nodes, used with -g")
+            EulerParser.inst.add_option("-d", dest="depth", type="int", default=2, help="depth, used with -g")
+            EulerParser.inst.add_option("-t", dest="relation", type="string", default="<",\
+                                              help="artifitial articulaiton rel, used with -g")
+            EulerParser.inst.add_option("-I", action="store_true", dest="incEx", default=False,\
+                                              help="generate an inconsistent example, used with -g")
+            EulerParser.inst.add_option("--iv", action="store_true", dest="inputViz", default=False,\
+                                              help="input visualization")
+            EulerParser.inst.add_option("--simpall", action="store_true", dest="simpAllView", default=False,\
                                   help="simplify the pw aggregate view")
-	return inst
-
-    instance = Callable(instance)
+	return EulerParser.inst
