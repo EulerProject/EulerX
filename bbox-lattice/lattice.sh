4e29899d1bfd3d2f113c273b1076b3aaa692bb4a
Shizhuo Yu <shizhuoyu@users.noreply.github.com>
2014-09-04 11:18:53 -0700

    fix some typo

--- bbox-lattice/lattice.sh
+++ bbox-lattice/lattice.sh
@@ -5,7 +5,7 @@ python awf.py -filter=i,o expWorlds.asp;
 echo "creating the lattice without color...";
 dlv -silent -filter=up  wexp-up.asp expWorlds_aw.asp > up.dlv;
 echo "running euler to get MIS...";
-euler -i $1.txt -e mnpw --ie > output.txt;
+euler -i $1.txt -e mnpw --repair=HST > output.txt;
 echo "from MIS to MAC and get lattice..."
 python lattice.py $1;
 dot -Tpdf $1_lat.dot -o $1_lat.pdf;
