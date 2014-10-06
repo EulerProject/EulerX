6060c044d19becae7ef4a5c1dde5152cf5e87b5f
pkianmajd <parisakianmajd@users.noreply.github.com>
2014-10-05 18:19:32 -0700

    extracted parser code from addID, addIsa, AddArt

---
+++ src-el/wizardParser.py
@@ -0,0 +1,14 @@
+# wizard parser
+import argparse
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-i","--iFile", action= "append")
+parser.add_argument("-t","--tFile", action= "append")
+parser.add_argument("-s","--sNum")
+args = parser.parse_args()
+if args.iFile == None:
+    args.iFile = "in.csv"
+if args.iFile == None:
+    args.iFile = " "
+if args.sNum == None:
+    args.sNum = 1
