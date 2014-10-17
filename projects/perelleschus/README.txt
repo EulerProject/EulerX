0a5d10b40181b141f0e0e559f590927c659f0479
Shizhuo Yu <shizhuoyu@gmail.com>
2014-10-16 17:19:39 -0700

    clean folder

---
+++ projects/perelleschus/README.txt
@@ -0,0 +1,6 @@
+This is used to create all figures in perelleschus example.
+Just run the following in terminal:
+
+./make-perelleschus-figures.sh
+
+You can also change the stylesheet files in stylesheets/ folder
---
+++ projects/perelleschus/make-perelleschus-figures.sh
@@ -0,0 +1,17 @@
+euler -i inputs/figure2.txt -e mnpw --rcgo ;
+euler -i inputs/figure4.txt -e mnpw --rcgo ;
+euler -i inputs/figure5.txt -e mnpw --rcgo ;
+euler -i inputs/figure6.txt -e mnpw --rcgo ;
+euler -i inputs/figure7.txt -e mnpw --rcgo ;
+euler -i inputs/figure8.txt -e mnpw --rcgo ;
+euler -i inputs/figure9.txt -e mnpw --rcgo ;
+euler -i inputs/figure10.txt -e mnpw --rcgo ;
+euler -i inputs/figure11.txt -e mnpw --rcgo ;
+euler -i inputs/figure12.txt -e mnpw --rcgo ;
+euler -i inputs/figure13.txt -e mnpw --rcgo ;
+euler -i inputs/figure14.txt -e mnpw --rcgo ;
+euler -i inputs/figure15.txt -e mnpw --rcgo ;
+euler -i inputs/figure16.txt -e mnpw --rcgo ;
+
+#==> in case oinputs/ inconsistency and other special cases the commands will be diinputs/inputs/erent!
+
