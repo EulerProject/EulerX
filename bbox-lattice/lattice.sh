069744ce43509e85f7fcfe5ff1af8966634958b5
Shizhuo Yu <shizhuoyu@gmail.com>
2014-10-23 21:27:51 -0700

    integrate diagnostic lattice and input wizard

--- bbox-lattice/lattice.sh
+++ bbox-lattice/lattice.sh
@@ -1,13 +1,19 @@
+curdir=$(pwd)
+euler=$(which euler)
+srcdir=$(dirname $euler)
+prodir=$(dirname $srcdir)
+cd $prodir
+cd bbox-lattice
+input=$curdir/$1
+
 echo "preprocessing..."
-python preprocess.py $1;
+echo "creating the lattice without color...";
+python preprocess.py $input;
 echo "saving all worlds...";
 python awf.py -filter=i,o expWorlds.asp;
-echo "creating the lattice without color...";
-dlv -silent -filter=up  wexp-up.asp expWorlds_aw.asp > up.dlv;
+#dlv -silent -filter=up  wexp-up.asp expWorlds_aw.asp > up.dlv;
 echo "running euler to get MIS...";
-euler -i $1.txt -e mnpw --repair=HST > output.txt;
+euler -i $input -e mnpw --repair=HST > output.txt;
 echo "from MIS to MAC and get lattice..."
-python lattice.py $1;
-dot -Tpdf $1_lat.dot -o $1_lat.pdf;
-dot -Tpdf $1_fulllat.dot -o $1_fulllat.pdf
+python lattice.py $input $curdir;
 echo "finish";
