898c13c757b23e17cac987b7676566e527eb5465
parisa <parisa.kianmajd@gmail.com>
2015-05-18 15:46:32 -0700

    changed euler diff to change the current directory only and save the timestamps in the beginning of the diff file

--- src-el/einputdiff.py
+++ src-el/einputdiff.py
@@ -1,5 +1,6 @@
 
-# Euler diff receives a path of Euler run as input and outputs a diff file for each pair of cleanTax files in that path 
+# Euler diff receives filename as input and outputs diff file showing the difference between the pair of files with this filename
+# in the current directory
 # __author__ = "Parisa Kianmajd"
 #__version__ = "1.0.1"
 
@@ -7,7 +8,7 @@ import difflib
 import sys
 import os
 
-def diff(f1,f2,t):
+def diff(f1,f2,t,c):
     filename = {f1:"", f2: ""}
     diff = difflib.ndiff(open(f1).readlines(), open(f2).readlines())
     for f in [f1,f2]:
@@ -16,7 +17,11 @@ def diff(f1,f2,t):
         else:
             filename[f] = f
         
-    out = open('diff_' + t + "_" + filename[f1].split(".")[0] + "_" + filename[f2] , "w")
+    out = open('diff_' + str(c) + "_" + filename[f1]  , "w")
+    out.write('timestamp #1:' + t[0] + '\n')
+    out.write('timestamp #2:' + t[1] + '\n')
+    out.write('\n')
+
     try:
         while 1:
             out.write(diff.next(),)
@@ -24,7 +29,7 @@ def diff(f1,f2,t):
         pass
     out.close()
 
-def findDiff():
+def einputdiff():
     filesList = list()
 
     try:
@@ -33,17 +38,19 @@ def findDiff():
         print "Usage: diff.py <arg1>"
         sys.exit(1)
 
-    rootdir = sys.argv[1]
+    filename = sys.argv[1]
 
-    for root, dirs, files in os.walk(rootdir):
+    for root, dirs, files in os.walk('.'):
         for name in files:
-            if name.endswith(".txt"):
-                timestamp = root.split("/")[-2]
+            if name == filename:
+                timestamp = root [2:20]
                 filesList.append([os.path.join(root, name),timestamp])
     i = 0
+    c = 1
     while i < len(filesList) - 1:
-        time = filesList[i][1] + "_" + filesList[i+1][1]
-        diff(filesList[i][0],filesList[i+1][0],time)
+        time = (filesList[i][1], filesList[i+1][1])
+        diff(filesList[i][0],filesList[i+1][0],time,c)
         i += 2
+        c += 1
 
-findDiff()
+einputdiff()
