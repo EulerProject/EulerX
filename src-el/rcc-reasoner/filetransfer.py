405071c63e561687fd95a78132d13faeef8c029d
Ryan <shizhuoyu@gmail.com>
2015-08-03 13:21:26 -0700

    transfer cleantax to ubermir file

--- src-el/rcc-reasoner/filetransfer.py
+++ src-el/rcc-reasoner/filetransfer.py
@@ -1,33 +1,6 @@
-# Copyright (c) 2014 University of California, Davis
-# 
-# 
-# Permission is hereby granted, free of charge, to any person obtaining
-# a copy of this software and associated documentation files (the
-# "Software"), to deal in the Software without restriction, including
-# without limitation the rights to use, copy, modify, merge, publish,
-# distribute, sublicense, and/or sell copies of the Software, and to
-# permit persons to whom the Software is furnished to do so, subject to
-# the following conditions:
-# 
-# The above copyright notice and this permission notice shall be
-# included in all copies or substantial portions of the Software.
-# 
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
-# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
-# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
-# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
-# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
-# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-#=============================================================================================
-# Main program: Read from CleanTax input file to generate a .csp file for PyRCC8
-# In order to convert RCC5 to RCC8, we join the tangential and non-tangential relations 
-#=============================================================================================
+#!/usr/bin/env python
 import sys
 import re
-import commands
-
 
 #remove the duplicate string in list li
 def remove_duplicate_string(li):
@@ -78,82 +51,23 @@ def replace_art_names(s):
     outputs = ""
     for e in inputs:
         if e == "equals":
-            outputs = outputs + "EQ "
+            outputs = outputs + "=,"
         elif e == "is_included_in":
-            outputs = outputs + "TPP NTPP "
+            outputs = outputs + "<,"
         elif e == "includes":
-            outputs = outputs + "TPPI NTPPI "
+            outputs = outputs + ">,"
         elif e == "overlaps":
-            outputs = outputs + "PO "
+            outputs = outputs + "o,"
         elif e == "disjoints":
-            outputs = outputs + "DC EC "
+            outputs = outputs + "!,"
     outputs = outputs[:-1]
     return outputs 
-#    if s == "equals":
-#        return "EQ"
-#    if s == "is_included_in":
-#        return "TPP NTPP"
-#    if s == "includes":
-#        return "TPPI NTPPI"
-#    if s == "overlaps":
-#        return "PO"
-#    if s == "disjoints":
-#        return "DC EC"
-
-def containLsumRsum(li):
-    for e in li:
-        if e == "rsum":
-            return "rsum"
-        if e == "lsum":
-            return "lsum"
-    return "none"
-
-
-def findConcept(n, rep_list):
-    for t in rep_list:
-        if n == t[1]:
-            return t[0] 
-    
-
-def findRelation(s):
-    relation = ""
-    if "DC" in s and "EC" in s:
-        relation = relation + "!"
-    if "EQ" in s:
-        relation = relation + "="
-    if "PO" in s:
-        relation = relation + "o"
-    if "TPP" in s and "NTPP" in s and "TPPI" not in s and "NTPPI" not in s:
-        relation = relation + "<"
-    if "TPPI" in s and "NTPPI" in s:
-        relation = relation + ">"
-    
-    relation2 = ""
-    if len(relation) > 1:
-        relation2 = "{"
-        for r in relation:
-            relation2 = relation2 + r + ", "
-        relation2 = relation2[:-2]+"}"
-    else:
-        relation2 = relation
-    
-    relation2 = relation2.replace("o","><")
-    return relation2
-
-def newgetoutput(cmd):
-    result = commands.getstatusoutput(cmd)
-    if result[0] != 0:
-        print "cmd: ", cmd
-        print "exit status: ", result[0]
-        return
-    else:
-        return result[1]
 
 # open the files
 fileName = sys.argv[1]
-pyrccFileName = fileName[:-4] + "_pyrcc.csp"
+uberMirFileName = fileName[:-4] + "_uber.txt"
 f_in = open(fileName, "r")
-f_out = open(pyrccFileName, "w")
+f_out = open(uberMirFileName, "w")
 
 # initialize variables
 pc_list1 =[]
@@ -164,7 +78,6 @@ replace_list = []
 children_list = []
 name1 = ""          # first taxonomy name
 name2 = ""          # second taxonomy name
-
 
 # read CleanTax input file
 lines = f_in.readlines()
@@ -221,40 +134,21 @@ for line in lines:
             art_type = art_type + temp_list[i] + " "
         art_item = [temp_list[0], temp_list[len(temp_list)-1], art_type[:-1]]
         art_list.append(art_item)
-        
-#print "pc_list1=", pc_list1
-#print "pc_list2=", pc_list2
-#print "art_list=", art_list
-
+            
 remove_duplicate_string(concept_list)
-#print "concept_list=", concept_list
 remove_duplicate_string(children_list)
-#print "children_list=", children_list
-#print "pc_list1=", pc_list1
-#print "pc_list2=", pc_list2
-#print "art_list=", art_list
-#print "children_list=", children_list
-
-
 replace_list = replace_names(concept_list, pc_list1, pc_list2, art_list, children_list)
-#print "AFTERRRRRR"
-#print "replace_list=", replace_list
-#print "pc_list1=", pc_list1
-#print "pc_list2=", pc_list2
-#print "art_list=", art_list
-#print "children_list=", children_list
+print "Hash of concepts:", replace_list
 
-
-### create the output file
 # write the title
-f_out.write(str(len(concept_list)-1) + " #" + fileName[:-4] + "\n")
+f_out.write(str(len(concept_list)) + "\n")
 
 # write the parent-child relation
 for e in pc_list1:
-    s = str(e[0]) + " " + str(e[1]) + " ( TPPI NTPPI )\n"
+    s = str(e[0]) + " " + str(e[1]) + " [>]\n"
     f_out.write(s)
 for e in pc_list2:
-    s = str(e[0]) + " " + str(e[1]) + " ( TPPI NTPPI )\n"
+    s = str(e[0]) + " " + str(e[1]) + " [>]\n"
     f_out.write(s)
 
 # update the art names in the articulation list
@@ -264,7 +158,7 @@ for e in art_list:
         temp_list = e[2].split(" ")
         for e2 in temp_list:
             e2 = replace_art_names(e2)
-            s = s + e2 + " "
+            s = s + e2 + ","
         e[2] = s[:-1]
     else:
         e[2] = replace_art_names(e[2])
@@ -272,47 +166,16 @@ for e in art_list:
 #print "modified_art_list=", art_list
 # write articulations
 for e in art_list:
-    s = str(e[0]) + " " + str(e[1]) + " ( " + e[2] + " )\n"
+    s = str(e[0]) + " " + str(e[1]) + " [" + e[2] + "]\n"
     f_out.write(s)
 #print "children_list=", children_list
 # write sibling-disjointness
 for children in children_list:
     for i in range(len(children)-1):
         for j in range(i+1,len(children)):
-            s = str(children[i]) + " " + str(children[j]) + " ( DC EC )\n"
+            s = str(children[i]) + " " + str(children[j]) + " [!]\n"
             f_out.write(s)
             
-f_out.write(".")
-
 
 f_in.close()
 f_out.close()
-
-# execute PyRCC8
-newgetoutput("rcc8 -d < " + pyrccFileName + " > output.txt")
-
-mir = []
-f = open("output.txt", "r")
-lines = f.readlines()
-
-for line in lines:
-    if '[' in line:
-        pair = []
-
-        rels = re.match('(.*)\s(.*)\s\[(.*)\](.*)', line)
-        
-        pair.append(findConcept(int(rels.group(1)), replace_list))
-        pair.append(findRelation(rels.group(3)))
-        pair.append(findConcept(int(rels.group(2)), replace_list))
-        
-        print pair
-        mir.append(pair)
-        
-
-#print mir
-
-
-newgetoutput("rm output.txt")
-
-
-
