498f670f35d2d67d41828672309b294f52ab5a35
Yilin Xia <xia.yl@outlook.com>
2023-06-02 04:25:02 +0000

    add devcontainer

---
+++ .devcontainer/postCreateCommand.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# This script sets up a python 2.7 environment and installs necessary tools
+
+echo "Creating a Python 2.7 environment named 'py2'"
+
+# Create a Python 2.7 environment
+conda create --name py2 python=2.7 --yes
+
+# Activate the Python 2.7 environment
+echo "Activating Python 2.7 environment"
+source activate py2
+
+# Unlink existing dlv binary
+echo "Removing existing DLV binary"
+sudo unlink /go/bin/dlv
+
+# Download the dlv binary
+echo "Downloading new DLV binary"
+wget https://www.dlvsystem.it/files/dlv.x86-64-linux-elf-static.bin
+
+# Change permissions to make the binary executable
+echo "Setting execute permissions for the DLV binary"
+chmod +x /workspaces/EulerX/dlv.x86-64-linux-elf-static.bin
+
+# Create a symbolic link to the binary
+echo "Creating symbolic link for the DLV binary"
+sudo ln -s /workspaces/EulerX/dlv.x86-64-linux-elf-static.bin /usr/local/bin/dlv
+
+# Install clingo and graphviz using conda
+echo "Installing clingo and graphviz"
+conda install -c potassco clingo --yes
+conda install -c anaconda graphviz --yes
+
+# Install Python dependencies
+echo "Installing Python dependencies"
+pip install docopt==0.6.1
+pip install pyyaml
+
+# Change permissions to make euler2 script executable
+echo "Setting execute permissions for euler2 script"
+chmod +x /workspaces/EulerX/src-el/euler2
+
+# Create a symbolic link to euler2 script
+echo "Creating symbolic link for euler2 script"
+sudo ln -s /workspaces/EulerX/src-el/euler2 /usr/local/bin/euler2
+
+echo "Setup completed successfully"
